ae3a3cb6914ca3730dd02ef369635c648141ece7
sri-ni-vas <srinivas.ibo.58@gmail.com>
2022-01-12 22:58:08 -0800

    first demo

---
+++ src/single/Demo1.java
@@ -0,0 +1,13 @@
+package single;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,30};
+		System.out.println("First value:"+a[0]);
+		System.out.println("First value:"+a[1]);
+		System.out.println("First value:"+a[2]);
+
+	}
+
+}
